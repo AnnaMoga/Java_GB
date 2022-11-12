650f933e92fe20bd70d1a097b34d58c8e80f5c51
AnnaMoga <96260026+AnnaMoga@users.noreply.github.com>
2022-11-12 22:56:16 +0300

    Выполнено ДЗ№3

---
+++ lesson3.java
@@ -0,0 +1,78 @@
+package lesson_3;
+
+import java.util.Arrays;
+
+public class lesson3 {
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+        Task5();
+    }
+
+    public static void Task1() {
+        System.out.println("Task 1");
+        int[] arr = {1, 0, 1, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void Task2() {
+        System.out.println("Task 2");
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void Task3() {
+        System.out.println("Task 3");
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void Task4() {
+        System.out.println("Task 4");
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Task5() {
+        System.out.println("Task 5");
+        int len = 10;
+        int initialValue = 2;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
